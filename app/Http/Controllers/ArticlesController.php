2a97ae77135cbdcf6e509cb1f1fd6e8529b69a5a
Nathan Johnson <nathancj224@gmail.com>
2015-08-31 23:35:04 -0400

    Improved delete page

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -39,7 +39,8 @@ class ArticlesController extends Controller
       return redirect('articles');
     }
 
-    public function delete($id) {
+    public function delete($id) 
+    {
       $article = $this->article->findOrFail($id);
       return view('articles.delete', ['article' => $article]);
     }
--- resources/views/articles/delete.blade.php
+++ resources/views/articles/delete.blade.php
@@ -8,17 +8,21 @@
 
   <hr />
 
-  <p>Really delete this article?</p>
+  <p>Do you really want to delete this article?</p>
 
-  <h3>{{$article->title}}</h3>
-  <p><em>{{$article->body}}</em></p>
+  <br />
+
+  <blockquote>
+    <h3>{{$article->title}}</h3>
+    <p><em>{{$article->body}}</em></p>
+  </blockquote>
 
   {!! Form::open([ 
       'method'=>'DELETE', 
       'url' => '/articles/'.$article->id, 
       'style'=>'display: inline'
   ]) !!}
-    <button type="submit" class="btn btn-danger">Yes, delete it</button> 
+    <button type="submit" class="btn btn-danger">Delete</button> 
     <a href="/articles/{{$article->id}}" class="btn btn-default">Cancel</a> 
   {!! Form::close() !!}
 
